bee3c438ccae12238b3edc04d72e3ad4d225d6eb
Aftab Sidhu <Aftab180@gmail.com>
2022-04-25 19:00:41 -0400

    commit tree c1227b8d861aad372eeb0824be2f54630e26f738

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = 62;
+
+const random = Math.floor(Math.random()*100)
+
+let num3 = 4;
+let num4 = 6;
+const mod = num3 % num4;
+
+let max = 20;
